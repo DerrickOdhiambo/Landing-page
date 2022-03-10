ba795e44a07076f81f34f48bf86b12bb7ea57283
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-03-10 13:52:28 +0300

    create the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import '../styles/header.scss';
 
 const Header = () => {
-	return <React.Fragment></React.Fragment>;
+	return (
+		<React.Fragment>
+			<section className='header'></section>
+		</React.Fragment>
+	);
 };
 
 export default Header;
