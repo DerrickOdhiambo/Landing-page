9f124bfa49ec4ce0f2ed5811e7d0f60aa4bd8e28
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-03-11 06:46:03 +0300

    create abouse component section and its css file

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/about.scss';
+
+const About = () => {
+	return (
+		<React.Fragment>
+			<section className='about'></section>
+		</React.Fragment>
+	);
+};
+
+export default About;
