0842e0209b658bae152cea5caf6e44e97248ca73
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-03-11 11:30:11 +0300

    link the achievement component to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header';
 import About from './components/About';
+import Achievement from './components/Achievement';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<About />
+			<Achievement />
 		</div>
 	);
 }
